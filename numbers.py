0ac262542cb8cd64d9c9361678d38f957a3df34f
David Cleary <davidcgcleary@gmail.com>
2023-03-29 22:22:05 +0000

    Updated numbers.py

--- numbers.py
+++ numbers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+import math
+
 def main():
     try:
         number = int(input("Enter a number: "))
         
         for n in range(1, number + 1):
-            print("The numbe is " + str(n))
+            print("The number is " + str(n) + ", its square is " + str(int(math.pow(n , 2))) + " and its cube is " + str(int(math.pow(n, 3))) + ".")
             
     except ValueError:
         print("ERROR: The value entered must be an integer.")
